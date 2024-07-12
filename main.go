ec1286ec81cee94a78529204279eed9ef64b5ac4
sadahal <103239663+sadahal@users.noreply.github.com>
2024-07-12 17:16:49 +1000

    test

--- main.go
+++ main.go
@@ -12,20 +12,20 @@ func main() {
 	fmt.Println(os.Getenv("KAFKA_BOOTSTRAP_SERVERS"))
 	kafkaConfig := kafka.LoadKafkaConfig()
 	log.Println("The kafka config is ", kafkaConfig)
-    // configMap := kafka.CreateKafkaConfigMap(kafkaConfig)
+    configMap := kafka.CreateKafkaConfigMap(kafkaConfig)
 
-    // adminClient, err := kafka.InitKafkaAdminClient(configMap)
-    // if err != nil {
-    //     panic(err)
-    // }
-    // defer adminClient.Close()
+    adminClient, err := kafka.InitKafkaAdminClient(configMap)
+    if err != nil {
+        panic(err)
+    }
+    defer adminClient.Close()
 
-    // topics, err := kafka.ListKafkaTopics(adminClient)
-    // if err != nil {
-    //     panic(err)
-    // }
+    topics, err := kafka.ListKafkaTopics(adminClient)
+    if err != nil {
+        panic(err)
+    }
 
-    // for _, topic := range topics {
-    //     fmt.Println(topic)
-    // }
+    for _, topic := range topics {
+        fmt.Println(topic)
+    }
 }
